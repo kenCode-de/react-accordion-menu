a31e52ab6a0621a82fd745b72acc43b7c45d45b2
Christian Fei <crifei93@gmail.com>
2015-08-23 13:11:12 +0200

    cleaned up tests

--- src/AccordionNode.test.js
+++ src/AccordionNode.test.js
@@ -10,21 +10,28 @@ describe('AccordionNode', function () {
     expect( elem.getAttribute('class') ).to.match(/collapsed/)
   })
   it('renders children', function () {
-    var child = <span className="child">child node</span>
-    var node = TestUtils.renderIntoDocument( <AccordionNode>{child}</AccordionNode> )
-    var elems = TestUtils.scryRenderedDOMComponentsWithClass(node, 'child')
+    var accordion = <AccordionNode>
+                      <div className="title">child node</div>
+                    </AccordionNode>
+    var node = TestUtils.renderIntoDocument( accordion )
+    var elems = TestUtils.scryRenderedDOMComponentsWithClass(node, 'title')
     expect( elems.length ).to.eql( 1 )
   })
   it('recursively renders AccordionNodes', function () {
-    var child = <AccordionNode>
-                  <span className="child">child node</span>
-                </AccordionNode>
-    var node = TestUtils.renderIntoDocument( <AccordionNode>{child}</AccordionNode> )
+    var accordion = <AccordionNode>
+                      <div className="title">child node 1</div>
+                      <div className="content">
+                        <AccordionNode>
+                          <div className="title">child node 2</div>
+                        </AccordionNode>
+                      </div>
+                    </AccordionNode>
+    var node = TestUtils.renderIntoDocument( accordion )
     var nodesElems = TestUtils.scryRenderedComponentsWithType(node, AccordionNode)
 
     expect( nodesElems.length ).to.eql( 2 )
 
-    var childElems = TestUtils.scryRenderedDOMComponentsWithClass(node, 'child')
+    var childElems = TestUtils.scryRenderedDOMComponentsWithClass(node, 'content')
     expect( childElems.length ).to.eql( 1 )
   })
 
