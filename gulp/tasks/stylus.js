4c7a90ab087e4d9d8f7a5ea5e39a6f722351acfd
Christian Fei <crifei93@gmail.com>
2015-08-26 19:32:11 +0200

    fixed issues
    - hover color for links
    - expanded first child

--- gulp/tasks/stylus.js
+++ gulp/tasks/stylus.js
@@ -3,6 +3,7 @@ var gulp = require('gulp')
   , stylus = require('gulp-stylus')
   , nib = require('nib')
   , rename = require('gulp-rename')
+  , minifyCss = require('gulp-minify-css')
 
 gulp.task('stylus', function(){
   return gulp.src(config.entryFiles.stylus)
@@ -11,6 +12,7 @@ gulp.task('stylus', function(){
       this.emit('end')
     })
     .pipe(stylus({use: [nib()]}))
+    .pipe(minifyCss({compatibility: 'ie8'}))
     .pipe(rename(config.files.module + '.css'))
     .pipe(gulp.dest(config.files.dist))
     .pipe(gulp.dest(config.files.example))
